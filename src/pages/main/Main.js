289b433d08f6c9c6b0734b066dd41b45c2f10434
hammer Q413 <fiyity@163.com>
2023-12-14 10:21:01 +0000

    hotitem 대신 productCart

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -6,12 +6,16 @@ import HotCard from "./Hotitem/HotCard";
 import Newitem from "./Newitem/Newitem";
 import Saleitem from "./Saleitem/Saleitem";
 import CategorySection from "../Category/Category";
+import ProductCard from "../../components/ProductCard";
 
 export default class Main extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
       showMoreExpanded: false,
+      items: [],
+      count: 0,
     };
   }
 
@@ -21,7 +25,138 @@ export default class Main extends React.Component {
     }));
   };
 
+  // UseEffect()랑 비슷
+  componentDidMount() {
+    this.fetchItems();
+  }
+
+  async fetchItems() {
+    try {
+      const token = localStorage.getItem("accessToken");
+
+      const response = await fetch(
+        `${process.env.REACT_APP_DONG10_BASEURL}/products/search?size=6&page=0&sortBy=stockLowToHigh`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (response.ok) {
+        const pageData = await response.json();
+        const items = pageData.content;
+        console.log(items)
+        if (Array.isArray(items)) {
+          this.setState({
+            items: items,
+            count: pageData.totalElements,
+          });
+        } else {
+          console.error("Data is not an array:", items);
+        }
+      } else {
+        console.error(
+          "Failed to fetch items:",
+          response.status,
+          response.statusText
+        );
+      }
+    } catch (error) {
+      console.error("Error during fetch:", error);
+    }
+  }
+
+  handleCartClick = async (item) => {
+    try {
+      const token = localStorage.getItem("accessToken");
+      const response = await fetch(
+        `${process.env.REACT_APP_DONG10_BASEURL}/carts/productNumber/${item.productNumber}`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (response.ok) {
+        alert(`${item.productTitle}이 카트에 추가됐습니다!`);
+      } else {
+        // Handle error response
+        console.error("Error:", response.statusText);
+      }
+    } catch (error) {
+      // Handle network error
+      console.error("Error:", error);
+    }
+  };
+
+  handleHeartClick = async (item) => {
+    if (item.isHeart === false) {
+      console.log("isHeart==false");
+      try {
+        const token = localStorage.getItem("accessToken");
+        const response = await fetch(
+          `${process.env.REACT_APP_DONG10_BASEURL}/hearts/product/${item.productNumber}`,
+          {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        if (response.ok) {
+          this.setState((prevState) => ({
+            items: prevState.items.map((prevItem) =>
+              prevItem.productNumber === item.productNumber
+                ? { ...prevItem, isHeart: true }
+                : prevItem
+            ),
+          }));
+        } else {
+          console.error("Error:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    } else {
+      try {
+        console.log("isHeart==true");
+        const token = localStorage.getItem("accessToken");
+        const response = await fetch(
+          `${process.env.REACT_APP_DONG10_BASEURL}/hearts/product/${item.productNumber}`,
+          {
+            method: "DELETE",
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        if (response.ok) {
+          this.setState((prevState) => ({
+            items: prevState.items.map((prevItem) =>
+              prevItem.productNumber === item.productNumber
+                ? { ...prevItem, isHeart: false }
+                : prevItem
+            ),
+          }));
+        } else {
+          console.error("Error:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+  };
+
   render() {
+
+    const { items } = this.state;
+
     return (
       <>
         <Nav />
@@ -36,10 +171,19 @@ export default class Main extends React.Component {
               </a>
             </div>
             <div className="HotItem">
-              <HotCard />
-              <HotCard />
-              <HotCard />
-              <HotCard />
+              {items.map((item) => (
+                // Use the properties of the heart.product object in the ProductCardSmall component
+                <ProductCard
+                  key={item.productNumber}
+                  name={item.productTitle}
+                  price={item.productPrice}
+                  stocks={item.productStock}
+                  imageUrl={item.productImageUrl}
+                  isHeart={item.isHeart}
+                  onCartClick={() => this.handleCartClick(item)}
+                  onHeartClick={() => this.handleHeartClick(item)}
+                />
+              ))}
             </div>
             <div className="SectionHeader">
               <a className="SectionTitle">신상품</a>
