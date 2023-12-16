6a03e1d25fcfd89d61c4304cb33e833a71fe6070
Chan <heochan01@korea.ac.kr>
2023-12-16 14:50:59 +0000

    Page Button Components - initial design

---
+++ src/components/PageBtnList.tsx
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+import PageButton from "./PageButton";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+
+const List = styled.div`
+  width: 100%;
+  padding: 12px 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+`;
+
+const FAIcon = styled(FontAwesomeIcon)`
+  width: 16px;
+  height: 16px;
+`;
+
+interface PageBtnListProps {
+  pageLength: number;
+}
+
+function PageBtnList({ pageLength }: PageBtnListProps) {
+  const arr = Array.from({ length: pageLength }, (v, i) => i + 1);
+  const [currentPage, setCurrentPage] = useState(1);
+  console.log(arr);
+
+  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    console.log(e);
+  };
+
+  return (
+    <List>
+      <FAIcon icon={faChevronLeft as IconProp} />
+      {arr.map((v) => (
+        <PageButton
+          key={v}
+          pageNum={v}
+          isCurrent={v === currentPage ? true : false}
+        />
+      ))}
+      <FAIcon icon={faChevronRight as IconProp} />
+    </List>
+  );
+}
+
+export default PageBtnList;
